c63b771fa2d11333768f0da852f982d32a955bff
IchikawaYukko <ichikawayuriko@yahoo.co.jp>
2018-08-29 00:04:17 +0900

    Implemented product_mix fetcher.

--- rr1.php
+++ rr1.php
@@ -8,10 +8,12 @@ $cookiefile = 'token.cookie';
 $conn = curl_init();
 $token = get_csrfmiddlewaretoken($token_url);
 get_auth_cookie($auth_url, $token);
-sendmail(get_target_package(get_pdf_url('sales_summary', 'lunch')));
+
+// TODO product_mix too
+sendmail($argv[1]);
 curl_close($conn);
 
-function get_pdf_url($type, $timeslot) {
+function get_range_by_timeslot($timeslot) {
   global $venue_name, $timezone;
 
   date_default_timezone_set($timezone);
@@ -22,22 +24,18 @@ function get_pdf_url($type, $timeslot) {
     case 'lunch':
       $time_begin = '03:00:01'; $time_end = '15:00:00';
       $range_from = urlencode($today).'+'.urlencode($time_begin);
-      $range_to	  = urlencode($today).'+'.urlencode($time_end);
       break;
     case 'tea':
       $time_begin = '15:00:01'; $time_end = '17:30:00';
       $range_from = urlencode($today).'+'.urlencode($time_begin);
-      $range_to   = urlencode($today).'+'.urlencode($time_end);
       break;
     case 'dinner':
       $time_begin = '17:30:01'; $time_end = '03:00:00';
       $range_from = urlencode($yesterday).'+'.urlencode($time_begin);
-      $range_to   = urlencode($today).'+'.urlencode($time_end);
       break;
   }
-
-  echo $test = "https://$venue_name.revelup.com/reports/$type/pdf/?dining_option=&employee=&online_app=&online_app_type=&online_app_platform=&show_unpaid=1&show_irregular=1&range_from=$range_from&range_to=$range_to";
-  return $test;
+  $range_to   = urlencode($today).'+'.urlencode($time_end);
+  return array('range_from' => $range_from, 'range_to' => $range_to);
 }
 
 function get_csrfmiddlewaretoken($url) {
@@ -84,10 +82,13 @@ function get_auth_cookie($url, $token) {
   curl_exec($conn);
 }
 
-function get_target_package($url) {
+function get_sales_summary($range_from, $range_to) {
   // download target PDF
-  global $conn, $cookiefile;
+  global $conn, $cookiefile, $venue_name;
 
+  $url = "https://$venue_name.revelup.com/reports/sales_summary/pdf/?dining_option=&employee=&online_app=&online_app_type=&online_app_platform=&show_unpaid=1&show_irregular=1&range_from=$range_from&range_to=$range_to";
+
+#die($url);
   curl_setopt_array($conn, array(
     CURLOPT_URL         => $url,
     CURLOPT_COOKIEJAR   => $cookiefile,
@@ -102,17 +103,61 @@ function get_target_package($url) {
   return curl_exec($conn);
 }
 
-function sendmail($filedata) {
+function get_product_mix($range_from, $range_to) {
+  global $cookiefile, $venue_name;
+
+  ob_start();
+  passthru("curl -sb $cookiefile -H 'Expect:' -d 'dining_option=' -d 'range_from=$range_from' -d 'range_to=$range_to' -d 'sort_by=' -d 'sort_reverse=' -d 'combo_expand=' -d 'employee=' -d 'online_app=' -d 'online_app_type=' -d 'online_app_platform=' -d 'dining_option=' -d 'show_unpaid=' -d 'show_irregular=' -d 'sort_view=1' -d 'show_class=1' -d 'quantity_settings=0' -d 'no-filter=0' -d 'day_of_week=' https://$venue_name.revelup.com/reports/product_mix/pdf/");
+  $output = ob_get_contents();
+//  $ob_end_clean();
+return $output;
+}
+
+/*function get_target_package_bypost($url) {
+  // download by POST
+  global $conn, $cookiefile, $token_url;
+
+$from = urlencode('08/21/2018 03:00:00');
+$to = urlencode('08/22/2018 03:00:00');
+ $postfields = "sort_by=&sort_reverse=&combo_expand=&employee=&online_app=&'online_app_type=&online_app_platform=&dining_option=&show_unpaid=1&show_irregular=1&sort_view=1&show_class=1&quantity_settings=0&no-filter=0&day_of_week=&range_from=$from&range_to=$to";
+
+  curl_setopt_array($conn, array(
+    CURLOPT_URL         => $url,
+    CURLOPT_COOKIEJAR   => $cookiefile,
+    CURLOPT_COOKIEFILE  => $cookiefile,
+    CURLOPT_POSTFIELDS  => $postfields,
+    CURLOPT_HTTPHEADER => array('Expect: '),
+    CURLOPT_POST => true,
+    CURLOPT_FAILONERROR => false,
+#    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_REFERER => $token_url.'reports/product_mix/',
+#    CURLOPT_AUTOREFERER => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_VERBOSE => true,
+#    CURLOPT_HEADER => true,
+#CURLINFO_HEADER_OUT => true
+));
+
+
+  return curl_exec($conn);
+}*/
+
+function sendmail($timeslot) {
 //  mb_language("Japanese");
 //  mb_internal_encoding("UTF-8");
   global $to_address,$from_address;
 
-  $filename = "hoge.pdf";
+  $today = date('d_m_Y');
+  $range = $range = get_range_by_timeslot($timeslot);
+  $filename1 = "SalesSummary$today.pdf";  //set correct name
+  $filedata1 = chunk_split(base64_encode(get_sales_summary($range['range_from'], $range['range_to'])));
+  $filename2 = "ProductMix$today.pdf";
+  $filedata2 = chunk_split(base64_encode(get_product_mix($range['range_from'], $range['range_to'])));
 
   $boundary = "__BOUNDARY__";
   $to		= $to_address;
   $from		= $from_address;
-  $subject	= "test";
+  $subject	= "$timeslot time Sales Summary, Product Mix";
   $headers	= "From: $from
 MIME-Version: 1.0
 Content-Type: multipart/mixed;boundary=\"$boundary\"
@@ -121,12 +166,21 @@ Content-Type: multipart/mixed;boundary=\"$boundary\"
   $message_body = "--$boundary
 Content-Type: text/plain; charset=\"ISO-2022-JP\"
 
-Today's summary and Product mix.
+Today's $timeslot time summary and Product mix.
 --$boundary
-Content-Type: application/octet-stream; name=\"{$filename}\"
-Content-Disposition: attachment; filename=\"{$filename}\"
-Content-Transfer-Encoding: base64\n\n";
-  $message_body .= chunk_split(base64_encode($filedata))."\n\n";
+Content-Type: application/octet-stream; name=\"{$filename1}\"
+Content-Disposition: attachment; filename=\"{$filename1}\"
+Content-Transfer-Encoding: base64
+
+$filedata1
+--$boundary
+Content-Type: application/octet-stream; name=\"{$filename2}\"
+Content-Disposition: attachment; filename=\"{$filename2}\"
+Content-Transfer-Encoding: base64
+
+$filedata2
+
+";
 #  $message_body .= "--$boundary--";
 //  echo $headers.$message_body;
   mail($to, $subject, $message_body, $headers);
