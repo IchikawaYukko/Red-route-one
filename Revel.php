460f4f30971b341d06add9d32dac8c84540cf3a9
IchikawaYukko <ichikawayuriko@yahoo.co.jp>
2018-09-13 00:32:42 +0900

    clean code. move code to Revel class.

---
+++ Revel.php
@@ -0,0 +1,174 @@
+<?php
+
+class Revel {
+    private $username, $password, $csrf_token, $curl_handle;
+    private $base_url, $auth_url;
+    const COOKIE_FILE = 'token.cookie';
+
+    public function __construct(string $username, string $password, string $venue_name) {
+        $this->username = $username;
+        $this->password = $password;
+
+        $this->base_url = "https://{$venue_name}.revelup.com/";
+        $this->auth_url = "{$this->base_url}login/?next=/dashboard/";
+
+        $this->curl_handle = curl_init();
+        $this->csrf_token = $this->get_csrfmiddlewaretoken();
+        $this->get_auth_cookie();
+    }
+
+    public function __destruct() {
+        curl_close($this->curl_handle);
+    }
+
+    private function get_csrfmiddlewaretoken() {
+        curl_setopt_array($this->curl_handle, array(
+            CURLOPT_URL		        => $this->base_url,
+            CURLOPT_COOKIEJAR	    => self::COOKIE_FILE,
+            CURLOPT_COOKIEFILE	    => self::COOKIE_FILE,
+            CURLOPT_FAILONERROR	    => true,
+            CURLOPT_FOLLOWLOCATION  => true,
+            CURLOPT_AUTOREFERER     => true,
+            /*CURLOPT_VERBOSE => true,*/
+            CURLOPT_RETURNTRANSFER  => true
+        ));
+      
+        $response = curl_exec($this->curl_handle) or die('fail on get_csrfmiddlewaretoken()');
+
+        // Parse HTML
+        $start = strpos($response, "csrfmiddlewaretoken' value='") + strlen("csrfmiddlewaretoken' value='");
+        $end = strpos($response, "'", $start);
+
+        // Find token
+        return substr($response, $start, $end - $start);
+    }
+
+    public static function get_range_by_timeslot(string $timeslot) {
+        date_default_timezone_set(TIME_ZONE);
+        $yesterday = date('m/d/Y', strtotime('-1 day'));
+        $today = date('m/d/Y');
+      
+        switch ($timeslot) {
+            case 'lunch':
+                $time_begin = '03:00:01'; $time_end = '15:00:00';
+                $range_from = urlencode($today).'+'.urlencode($time_begin);
+                break;
+            case 'tea':
+                $time_begin = '15:00:01'; $time_end = '17:30:00';
+                $range_from = urlencode($today).'+'.urlencode($time_begin);
+                break;
+            case 'dinner':
+                $time_begin = '17:30:01'; $time_end = '03:00:00';
+                $range_from = urlencode($yesterday).'+'.urlencode($time_begin);
+                break;
+        }
+        $range_to   = urlencode($today).'+'.urlencode($time_end);
+        return array('range_from' => $range_from, 'range_to' => $range_to);
+    }
+
+    public static function get_filename_suffix_by_timeslot(string $timeslot) {
+        date_default_timezone_set(TIME_ZONE);
+        $yesterday = date('m/d/Y', strtotime('-1 day'));
+        $today = date('m/d/Y');
+      
+        switch ($timeslot) {
+            case 'lunch':
+                $filedate = $today;
+                break;
+            case 'tea':
+                $filedate = $today;
+                break;
+            case 'dinner':
+                $filedate = $yesterday;
+                break;
+        }
+        return $filedate.$timeslot;
+    }
+
+    // Get Auth Cookie (Cookie will be saved as file (not return value))
+    private function get_auth_cookie() {
+        $postfields = http_build_query([
+            'username' => $this->username,
+            'password' => $this->password,
+            'csrfmiddlewaretoken' => $this->csrf_token
+        ]);
+        curl_setopt_array($this->curl_handle, array(
+            CURLOPT_URL         => $this->auth_url,
+            CURLOPT_COOKIEJAR   => self::COOKIE_FILE,
+            CURLOPT_COOKIEFILE  => self::COOKIE_FILE,
+            CURLOPT_POST	    => false,
+            CURLOPT_POSTFIELDS  => $postfields,
+            CURLOPT_FAILONERROR => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_REFERER     => $this->auth_url,
+            CURLOPT_AUTOREFERER => true,
+            CURLOPT_RETURNTRANSFER => true
+        ));
+        
+        curl_exec($this->curl_handle);
+    }
+
+    // Download and return Sales Summary PDF
+    public function get_sales_summary($range_from, $range_to) {
+        $url = "{$this->base_url}reports/sales_summary/pdf/?dining_option=&employee=&online_app=&online_app_type=&online_app_platform=&show_unpaid=1&show_irregular=1&range_from=$range_from&range_to=$range_to";
+
+        curl_setopt_array($this->curl_handle, array(
+            CURLOPT_URL         => $url,
+            CURLOPT_COOKIEJAR   => self::COOKIE_FILE,
+            CURLOPT_COOKIEFILE  => self::COOKIE_FILE,
+            CURLOPT_FAILONERROR => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_REFERER     => $url,
+            CURLOPT_AUTOREFERER => true,
+            /*CURLOPT_VERBOSE => true,*/
+            CURLOPT_RETURNTRANSFER => true
+        ));
+        return curl_exec($this->curl_handle);
+    }
+
+    // Download and return Product Mix PDF
+    public function get_product_mix($range_from, $range_to) {
+        $url = "{$this->base_url}reports/product_mix/pdf/";
+
+        $report_option = 
+            'dining_option='
+            .'&range_from='.$range_from
+            .'&range_to='.$range_to
+            .'&sort_by=n_items'
+            .'&sort_reverse='
+            .'&combo_expand='
+            .'&employee='
+            .'&online_app='
+            .'&online_app_type='
+            .'&online_app_platform='
+            .'&show_unpaid=1'
+            .'&show_irregular=1'
+            .'&sort_view=2'
+            .'&show_product=1'
+            .'&quantity_settings=0'
+            .'&no-filter=0'
+            .'&day_of_week='
+        ;
+        $request_header = array(
+            'Content-Type: application/x-www-form-urlencoded',
+            'Expect:'
+        );
+        curl_setopt_array($this->curl_handle, array(
+            CURLOPT_URL             => $url,
+            CURLOPT_COOKIEJAR       => self::COOKIE_FILE,
+            CURLOPT_COOKIEFILE      => self::COOKIE_FILE,
+            CURLOPT_FAILONERROR     => true,
+            CURLOPT_FOLLOWLOCATION  => true,
+            CURLOPT_REFERER         => $url,
+            CURLOPT_POST            => true,
+            CURLOPT_POSTFIELDS      => $report_option,
+            CURLOPT_HTTPHEADER      => $request_header,
+            CURLOPT_AUTOREFERER     => true,
+            //CURLOPT_VERBOSE     => true,
+            CURLOPT_RETURNTRANSFER  => true
+        ));
+
+        return curl_exec($this->curl_handle);
+    }
+}
+?>
--- rr1.php
+++ rr1.php
@@ -1,263 +1,56 @@
 <?php
 require_once("settings.php");
 require_once("RR1_Mail.php");
+require_once("Revel.php");
 
-define('BASE_URL', 'https://'.VENUE_NAME.'.revelup.com/');
-define('AUTH_URL', BASE_URL.'login/?next=/dashboard/');
-define('COOKIE_FILE', 'token.cookie');
+main($argv[1]);
 
-$conn = curl_init();
-$token = get_csrfmiddlewaretoken(BASE_URL);
-get_auth_cookie(AUTH_URL, $token);
-
-$timeslot = $argv[1];
-//sendmail($argv[1]);
-
-$addr = array(
-  'to'        =>  TO_ADDRESS,
-  'from'      =>  FROM_ADDRESS,
-  'reply_to'  =>  REPLY_TO_ADDRESS,
-);
-
-$today = date('d_m_Y');
-$range = get_range_by_timeslot($timeslot);
-
-$file = array();
-$file[] = array(
-  'filename'  =>  "SalesSummary$today.pdf",  // TODO set correct name
-  'data'      =>  get_sales_summary($range['range_from'], $range['range_to']),
-);
-
-$file[] = array(
-  'filename'  =>  "ProductMix$today.pdf",
-  'data'      =>  get_product_mix($range['range_from'], $range['range_to']),
-);
-
-$subject	= "$timeslot time Sales Summary, Product Mix";
-$message = "Today's $timeslot time summary and Product mix.";
-$message .= $body_footer;
-
-$mail = new RR1Mail();
-$mail->sendmail($addr, $subject, $message, $file);
-
-curl_close($conn);
-
-function get_range_by_timeslot($timeslot) {
-  date_default_timezone_set(TIME_ZONE);
-  $yesterday = date('m/d/Y', strtotime('-1 day'));
-  $today = date('m/d/Y');
-  //$today = '09/10/2018';
-
-  switch ($timeslot) {
-    case 'lunch':
-      $time_begin = '03:00:01'; $time_end = '15:00:00';
-      $range_from = urlencode($today).'+'.urlencode($time_begin);
-      break;
-    case 'tea':
-      $time_begin = '15:00:01'; $time_end = '17:30:00';
-      $range_from = urlencode($today).'+'.urlencode($time_begin);
-      break;
-    case 'dinner':
-      $time_begin = '17:30:01'; $time_end = '03:00:00';
-      $range_from = urlencode($yesterday).'+'.urlencode($time_begin);
-      break;
-  }
-  $range_to   = urlencode($today).'+'.urlencode($time_end);
-  return array('range_from' => $range_from, 'range_to' => $range_to);
+function main(string $timeslot) {
+    $attach_file = download($timeslot);
+    send($attach_file, $timeslot);
 }
 
-function get_csrfmiddlewaretoken($url) {
-  global $conn;
+function download(string $timeslot) {
+    $revel = new Revel(REVEL_USERNAME, REVEL_PASSWORD, VENUE_NAME);
 
-  curl_setopt_array($conn, array(
-    CURLOPT_URL		=> $url,
-    CURLOPT_COOKIEJAR	=> COOKIE_FILE,
-    CURLOPT_COOKIEFILE	=> COOKIE_FILE,
-    CURLOPT_FAILONERROR	=> true,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_AUTOREFERER => true,
-    /*CURLOPT_VERBOSE => true,*/
-    CURLOPT_RETURNTRANSFER => true
-  ));
+    $range = $revel->get_range_by_timeslot($timeslot);
+    $filesuffix = $revel->get_filename_suffix_by_timeslot($timeslot);
 
-  $response = curl_exec($conn) or die('fail on get_csrfmiddlewaretoken()');
-  /*var_dump($response);*/
-  $start = strpos($response, "csrfmiddlewaretoken' value='") + strlen("csrfmiddlewaretoken' value='");
-  $end = strpos($response, "'", $start);
+    $file = array();
+    if($timeslot == 'lunch' || $timeslot == 'tea') {
+      $file[] = array(
+        'filename'  =>  "SalesSummary{$filesuffix}.pdf",
+        'data'      =>  $revel->get_sales_summary($range['range_from'], $range['range_to']),
+      );
+    }
 
-  return substr($response, $start, $end - $start);
+    $file[] = array(
+      'filename'  =>  "ProductMix{$filesuffix}.pdf",
+      'data'      =>  $revel->get_product_mix($range['range_from'], $range['range_to']),
+    );
+
+    return $file;
 }
 
-function get_auth_cookie($url, $token) {
-  global $conn;
+function send(array $file, string $timeslot) {
+    global $body_footer;
 
-  $postfields = http_build_query([
-    'username' => REVEL_USERNAME,
-    'password' => REVEL_PASSWORD,
-    'csrfmiddlewaretoken' => $token
-  ]);
-  curl_setopt_array($conn, array(
-    CURLOPT_URL         => $url,
-    CURLOPT_COOKIEJAR   => COOKIE_FILE,
-    CURLOPT_COOKIEFILE  => COOKIE_FILE,
-    CURLOPT_POST	=> false,
-    CURLOPT_POSTFIELDS  => $postfields,
-    CURLOPT_FAILONERROR => true,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_REFERER => $url,
-    CURLOPT_AUTOREFERER => true,
-    CURLOPT_RETURNTRANSFER => true
-));
+    $addr = array(
+      'to'        =>  TO_ADDRESS,
+      'from'      =>  FROM_ADDRESS,
+      'reply_to'  =>  REPLY_TO_ADDRESS,
+    );
 
-  curl_exec($conn);
+    if($timeslot == 'lunch' || $timeslot == 'tea') {
+        $subject	= "$timeslot time Sales Summary, Product Mix";
+        $message = "Today's $timeslot time Sales Summary and Product mix.";
+    } else {
+        $subject	= "$timeslot time Product Mix";
+        $message = "Today's $timeslot time Product mix.";
+    }
+    $message .= $body_footer;
+
+    $mail = new RR1Mail();
+    $mail->sendmail($addr, $subject, $message, $file);
 }
-
-function get_sales_summary($range_from, $range_to) {
-  // download target PDF
-  global $conn, $base_url;
-
-  $url = BASE_URL."reports/sales_summary/pdf/?dining_option=&employee=&online_app=&online_app_type=&online_app_platform=&show_unpaid=1&show_irregular=1&range_from=$range_from&range_to=$range_to";
-
-  curl_setopt_array($conn, array(
-    CURLOPT_URL         => $url,
-    CURLOPT_COOKIEJAR   => COOKIE_FILE,
-    CURLOPT_COOKIEFILE  => COOKIE_FILE,
-    CURLOPT_FAILONERROR => true,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_REFERER => $url,
-    CURLOPT_AUTOREFERER => true,
-    /*CURLOPT_VERBOSE => true,*/
-    CURLOPT_RETURNTRANSFER => true
-));
-
-  /*
-  $result = curl_exec($conn);
-  echo $result;
-  return $result;
-  */
-  return curl_exec($conn);
-}
-
-/*
-function get_product_mix_by_external_curl($range_from, $range_to) {
-  global $cookiefile, $venue_name, $base_url;
-
-  ob_start() or die('error on ob_start');
-  passthru("curl -sb $cookiefile -H 'Expect:' -d 'dining_option=' -d 'range_from=$range_from' -d 'range_to=$range_to' -d 'sort_by=n_items' -d 'sort_reverse=' -d 'combo_expand=' -d 'employee=' -d 'online_app=' -d 'online_app_type=' -d 'online_app_platform=' -d 'dining_option=' -d 'show_unpaid=' -d 'show_irregular=' -d 'sort_view=2' -d 'show_class=1' -d 'quantity_settings=0' -d 'no-filter=0' -d 'day_of_week=' ".$base_url."reports/product_mix/pdf/");
-  $output = ob_get_flush();
-//  $ob_end_clean();
-  return $output;
-}
-*/
-
-function get_product_mix($range_from, $range_to) {
-  global $conn;
-
-  $url = BASE_URL.'reports/product_mix/pdf/';
-  /*$report_option = array(
-    'dining_option' => '',
-    'range_from'    => urldecode($range_from),   // UNDONE urldecode?
-    'range_to'      => urldecode($range_to),
-    'sort_by'       => 'n_items',
-    'sort_reverse'  => '',
-    'combo_expand'  => '',
-    'employee'      => '',
-    'online_app'    => '',
-    'online_app_type' => '',
-    'online_app_platform' => '',
-    'show_unpaid'   => '',
-    'show_irregular' => '',
-    'sort_view'     => '2',
-    'show_class'    => '1',
-    'quantity_settings' => '0',
-    'no-filter'     => '0',
-    'day_of_week'   => ''
-  );*/
-
-  $report_option = 
-    'dining_option='
-    .'&range_from='.$range_from
-    .'&range_to='.$range_to
-    .'&sort_by=n_items'
-    .'&sort_reverse='
-    .'&combo_expand='
-    .'&employee='
-    .'&online_app='
-    .'&online_app_type='
-    .'&online_app_platform='
-    .'&show_unpaid=1'
-    .'&show_irregular=1'
-    .'&sort_view=2'
-    .'&show_product=1'
-    .'&quantity_settings=0'
-    .'&no-filter=0'
-    .'&day_of_week='
-  ;
-  $request_header = array(
-    'Content-Type: application/x-www-form-urlencoded',
-    'Expect:'
-  );
-  curl_setopt_array($conn, array(
-    CURLOPT_URL         => $url,
-    CURLOPT_COOKIEJAR   => COOKIE_FILE,
-    CURLOPT_COOKIEFILE  => COOKIE_FILE,
-    CURLOPT_FAILONERROR => true,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_REFERER     => $url,
-    CURLOPT_POST        => true,
-    CURLOPT_POSTFIELDS  => $report_option,
-    CURLOPT_HTTPHEADER  => $request_header,
-    CURLOPT_AUTOREFERER => true,
-    //CURLOPT_VERBOSE     => true,
-    CURLOPT_RETURNTRANSFER => true
-));
-
-  return curl_exec($conn);
-}
-
-/*function sendmail($timeslot) {
-  global $body_text,$body_footer;
-
-  $today = date('d_m_Y');
-  $range = $range = get_range_by_timeslot($timeslot);
-  $filename1 = "SalesSummary$today.pdf";  //set correct name
-  $filedata1 = chunk_split(base64_encode(get_sales_summary($range['range_from'], $range['range_to'])));
-  $filename2 = "ProductMix$today.pdf";
-  $filedata2 = chunk_split(base64_encode(get_product_mix($range['range_from'], $range['range_to'])));
-
-  $boundary = "__BOUNDARY__";
-  $to		= TO_ADDRESS;
-  $from		= FROM_ADDRESS;
-  $reply_to = REPLY_TO_ADDRESS;
-  $subject	= "$timeslot time Sales Summary, Product Mix";
-  $headers	= "From: $from
-Reply-to: $reply_to
-MIME-Version: 1.0
-Content-Type: multipart/mixed;boundary=\"$boundary\"
-";
-
-  $message_body = "--$boundary
-Content-Type: text/plain; charset=\"ISO-2022-JP\"
-
-Today's $timeslot time summary and Product mix.
-
-$body_footer
---$boundary
-Content-Type: application/octet-stream; name=\"{$filename1}\"
-Content-Disposition: attachment; filename=\"{$filename1}\"
-Content-Transfer-Encoding: base64
-
-$filedata1
---$boundary
-Content-Type: application/octet-stream; name=\"{$filename2}\"
-Content-Disposition: attachment; filename=\"{$filename2}\"
-Content-Transfer-Encoding: base64
-
-$filedata2
-
-";
-  $message_body .= "--$boundary--";
-
-  mail($to, $subject, $message_body, $headers);
-}*/
 ?>
